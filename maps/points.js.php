a31a735bce27b3373d56fe4f074a4eba2971c655
Peter Sparrow <peter@thesparrows.net>
2026-01-07 10:56:39 +0000

    Bug fix - points getting loaded twice causing memory overflow on large time ranges

--- maps/points.js.php
+++ maps/points.js.php
@@ -8,20 +8,20 @@ $mapID = $_GET["mapID"];
 $name = GetMapName($mapID);
 $markers = GetMarkers($_GET["mapID"]);
 
-//get routes and points on the map
-$points = GetPoints($_GET["mapID"],$day,$duration);
-$routes = GetRoutes($_GET["mapID"],$day,$duration);
-
 $finalRoutes = [];
 
-foreach ($routes as $route) {
-    $routePoints = GetPoints($_GET["mapID"],$route[1],$route[1]-$route[0]);
-    $finalRoute = [$route,$routePoints];
-    array_push($finalRoutes, $finalRoute);
-}
-
 if (isset($_GET["RAW"])) {
+    $points = GetPoints($_GET["mapID"],$day,$duration);
     $finalRoutes = [[[$points[count($points)-1][2],$points[0][2],0],$points]];
+} else {
+    //get routes on the map
+    $routes = GetRoutes($_GET["mapID"],$day,$duration);
+    
+    foreach ($routes as $route) {
+        $routePoints = GetPoints($_GET["mapID"],$route[1],$route[1]-$route[0]);
+        $finalRoute = [$route,$routePoints];
+        array_push($finalRoutes, $finalRoute);
+    }
 }
 
 if (!$permissions[0]){
@@ -47,6 +47,9 @@ $result = array(
 );
 if ($permissions[0]){$result["markers"] = $markers;}
 if ($permissions[1]){$result["last"] = $last;}
+
+//close db to save memory before encoding
+$db->close();
 
 //json encode result
 echo json_encode($result);
